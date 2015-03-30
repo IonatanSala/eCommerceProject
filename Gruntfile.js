d891067815dbcb58b49764a0daeea42406b53463
Ionatan Sala <sala.jonatan1995@gmail.com>
2015-03-30 20:01:09 +0100

    idea for navbar

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,18 @@ module.exports = function(grunt) {
 
 		// auto prefix the css
 		autoprefixer: {
+			options: {
+        		browsers: ['> 1%', 'last 2 versions', 'Firefox >= 20', 'Opera 12.1']
+     		 },
 			mutiple_files: {
 				expand: true,
 				flatten: true,
 				src: 'app/css/*.css',
-				dest: 'dist/css/'
+				dest: 'app/css/'
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 };
